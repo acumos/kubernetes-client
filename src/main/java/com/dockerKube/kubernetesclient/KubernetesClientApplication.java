e3fcaed114296cb729373469e75945ce5e8798b0
Santosh Kumar18 <SK00472486@TechMahindra.com>
2018-09-13 21:16:18 +0530

    Set path for environment variables
    Change-Id: Ic3f7f4d6e3bf1cbb7a67aacec0ff777ec3708190
    Issue-ID: ACUMOS-1289
    Signed-off-by: Santosh Kumar18 <SK00472486@TechMahindra.com>

--- src/main/java/com/dockerKube/kubernetesclient/KubernetesClientApplication.java
+++ src/main/java/com/dockerKube/kubernetesclient/KubernetesClientApplication.java
@@ -23,11 +23,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dockerKube.*"})
 public class KubernetesClientApplication {
-
-	public static void main(String[] args) {
+	public static final String CONFIG_ENV_VAR_NAME = "SPRING_APPLICATION_JSON";
+	public static void main(String[] args)throws Exception {
+		final String springApplicationJson = System.getenv(CONFIG_ENV_VAR_NAME);
+		if (springApplicationJson != null && springApplicationJson.contains("{")) {
+			final ObjectMapper mapper = new ObjectMapper();
+			// ensure it's valid
+			mapper.readTree(springApplicationJson);
+			// logger.info("main: successfully parsed configuration from environment {}",
+			// CONFIG_ENV_VAR_NAME);
+		} else {
+			// logger.warn("main: no configuration found in environment {}",
+			// CONFIG_ENV_VAR_NAME);
+		}
+		
 		SpringApplication.run(KubernetesClientApplication.class, args);
 	}
 }
