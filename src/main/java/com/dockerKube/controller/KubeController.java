f6fc65fd0c0df940b86d6d2e527de89d4fdcf513
Santosh Kumar18 <SK00472486@TechMahindra.com>
2018-09-20 16:53:12 +0530

    Create getsolutionZip api for single and composite
    Change-Id: I396f5e4222d4e4a951b6779f1246522fa196a297
    Issue-ID: ACUMOS-1289
    Signed-off-by: Santosh Kumar18 <SK00472486@TechMahindra.com>

--- src/main/java/com/dockerKube/controller/KubeController.java
+++ src/main/java/com/dockerKube/controller/KubeController.java
@@ -20,10 +20,7 @@
 package com.dockerKube.controller;
 
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -32,15 +29,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dockerKube.beans.ContainerBean;
 import com.dockerKube.beans.DeploymentBean;
 import com.dockerKube.service.KubeService;
 import com.dockerKube.utils.DockerKubeConstants;
-import com.dockerKube.utils.ParseJSON;
 
 
 @RestController
@@ -54,138 +50,113 @@ public class KubeController {
 	
 	Logger log = LoggerFactory.getLogger(KubeController.class);
 	
-	/**
-	    * getSingleSolutionZip is used for single model deployment 
-	    * @param solutionId 
-	    *           - solutionId for Model 
-	    * @param solutionRevisionId
-	    *           - solutionRevisionId for Model 
-	    * @param imageTag
-	    *              - image name                
-	    * @param response
-	    *           - response return file
-	    * @throws IOException
-	    *             exception thrown
-	    */
 	
-	@RequestMapping(value = "/getSingleSolutionZip", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public void getSingleSolutionZip( String solutionId, String solutionRevisionId,String imageTag,HttpServletResponse response) throws IOException {
-		log.debug("Start simple solution ");
-		byte[] solutionZip=null;
-		DeploymentBean dBean=new DeploymentBean();
-		try{
-			String singleModelPort=(env.getProperty(DockerKubeConstants.SINGLE_MODEL_PORT) != null) ? env.getProperty(DockerKubeConstants.SINGLE_MODEL_PORT) : "";
-			String folderPath=(env.getProperty(DockerKubeConstants.FOLDERPATH) != null) ? env.getProperty(DockerKubeConstants.FOLDERPATH) : "";
-			log.debug("singleModelPort "+singleModelPort);
-			log.debug("folderPath "+folderPath);
-			String solutionYaml=kubeService.getSingleSolutionYMLFile(imageTag,singleModelPort);
-			dBean.setSolutionYml(solutionYaml);
-			dBean.setFolderPath(folderPath);
-			solutionZip=kubeService.createSingleSolutionZip(dBean);
-			
-		}catch(Exception e){
-			log.error("getSingleSolutionZip failed", e);
+	@RequestMapping(value = "/getSolutionZip/{solutionId}/{revisionId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public void getSolutionZip(@PathVariable("solutionId") String solutionId, @PathVariable("revisionId") String revisionId,HttpServletResponse response) throws IOException {
+		 log.debug("Start getSolutionZip solutionId"+solutionId+" revisionId "+revisionId);
+		 DeploymentBean dBean=new DeploymentBean();
+		 byte[] solutionZip=null;
+		 String singleModelPort=(env.getProperty(DockerKubeConstants.SINGLE_MODEL_PORT) != null) ? env.getProperty(DockerKubeConstants.SINGLE_MODEL_PORT) : "";
+		 String singleNodePort=(env.getProperty(DockerKubeConstants.SINGLE_NODE_PORT) != null) ? env.getProperty(DockerKubeConstants.SINGLE_NODE_PORT) : "";
+		 String singleTargetPort=(env.getProperty(DockerKubeConstants.SINGLE_TARGET_PORT) != null) ? env.getProperty(DockerKubeConstants.SINGLE_TARGET_PORT) : "";
+		 String nexusUrl=(env.getProperty(DockerKubeConstants.NEXUS_URL_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_URL_PROP) : "";
+	   	 String nexusUsername=(env.getProperty(DockerKubeConstants.NEXUS_USERNAME_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_USERNAME_PROP) : "";
+	   	 String nexusPd=(env.getProperty(DockerKubeConstants.NEXUS_PD_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_PD_PROP) : "";
+	   	 String cmnDataUrl=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCENDPOINTURL_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCENDPOINTURL_PROP) : "";
+	   	 String cmnDataUser=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCUSER_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCUSER_PROP) : "";
+	   	 String cmnDataPd=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCPD_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCPD_PROP) : "";
+	   	 String bluePrintImage=(env.getProperty(DockerKubeConstants.BLUEPRINT_IMAGENAME_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_IMAGENAME_PROP):"";
+	   	 String bluePrintName=(env.getProperty(DockerKubeConstants.BLUEPRINT_NAME_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_NAME_PROP):"";
+	   	 String bluePrintPort=(env.getProperty(DockerKubeConstants.BLUEPRINT_PORT_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_PORT_PROP):"";
+	   	 String kubeIP=(env.getProperty(DockerKubeConstants.KUBE_IP)!= null) ?env.getProperty(DockerKubeConstants.KUBE_IP):"";
+	   	 String probeImage=(env.getProperty(DockerKubeConstants.PROBEIMAGE_NAME)!= null) ?env.getProperty(DockerKubeConstants.PROBEIMAGE_NAME):"";
+	   	 String probePort=(env.getProperty(DockerKubeConstants.PROBEIMAGE_PORT)!= null) ?env.getProperty(DockerKubeConstants.PROBEIMAGE_PORT):"";
+	   	 String incrementPort=(env.getProperty(DockerKubeConstants.INCREMENT_PORT)!= null) ?env.getProperty(DockerKubeConstants.INCREMENT_PORT):"";
+	   	 String folderPath=(env.getProperty(DockerKubeConstants.FOLDERPATH) != null) ? env.getProperty(DockerKubeConstants.FOLDERPATH) : "";
+	   	 String bluePrintNodePort=(env.getProperty(DockerKubeConstants.BLUEPRINT_NODEPORT_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_NODEPORT_PROP):"";
+	   	 String mlTargetPort=(env.getProperty(DockerKubeConstants.ML_TARGET_PORT)!= null) ?env.getProperty(DockerKubeConstants.ML_TARGET_PORT):"";
+	   	 String dataBrokerModelPort=(env.getProperty(DockerKubeConstants.DATABROKER_MODEL_PORT) != null) ? env.getProperty(DockerKubeConstants.DATABROKER_MODEL_PORT) : "";
+		 String dataBrokerNodePort=(env.getProperty(DockerKubeConstants.DATABROKER_NODE_PORT) != null) ? env.getProperty(DockerKubeConstants.DATABROKER_NODE_PORT) : "";
+		 String dataBrokerTargetPort=(env.getProperty(DockerKubeConstants.DATABROKER_TARGET_PORT) != null) ? env.getProperty(DockerKubeConstants.DATABROKER_TARGET_PORT) : "";
+		 
+		 String probeModelPort=(env.getProperty(DockerKubeConstants.PROBE_MODEL_PORT) != null) ? env.getProperty(DockerKubeConstants.PROBE_MODEL_PORT) : "";
+		 String probeNodePort=(env.getProperty(DockerKubeConstants.PROBE_NODE_PORT) != null) ? env.getProperty(DockerKubeConstants.PROBE_NODE_PORT) : "";
+		 String probeTargetPort=(env.getProperty(DockerKubeConstants.PROBE_TARGET_PORT) != null) ? env.getProperty(DockerKubeConstants.PROBE_TARGET_PORT) : "";
+		 String probeApiPort=(env.getProperty(DockerKubeConstants.PROBE_API_PORT) != null) ? env.getProperty(DockerKubeConstants.PROBE_API_PORT) : "";
+		 
+	   	 dBean.setSolutionId(solutionId);
+    	 dBean.setSolutionRevisionId(revisionId);
+    	 dBean.setNexusUrl(nexusUrl);
+    	 dBean.setNexusUserName(nexusUsername);
+    	 dBean.setNexusPd(nexusPd);
+    	 dBean.setCmnDataUrl(cmnDataUrl);
+    	 dBean.setCmnDataUser(cmnDataUser);
+    	 dBean.setCmnDataPd(cmnDataPd);
+    	 dBean.setBluePrintImage(bluePrintImage);
+    	 dBean.setBluePrintName(bluePrintName);
+    	 dBean.setBluePrintPort(bluePrintPort);
+    	 dBean.setKubeIP(kubeIP);
+    	 dBean.setProbeImage(probeImage);
+    	 dBean.setProbePort(probePort);
+    	 dBean.setIncrementPort(incrementPort);
+    	 dBean.setFolderPath(folderPath);
+    	 dBean.setSingleModelPort(singleModelPort);
+    	 dBean.setSingleNodePort(singleNodePort);
+    	 dBean.setSingleTargetPort(singleTargetPort);
+    	 dBean.setBluePrintNodePort(bluePrintNodePort);
+    	 dBean.setMlTargetPort(mlTargetPort);
+    	 dBean.setDataBrokerModelPort(dataBrokerModelPort);
+    	 dBean.setBluePrintNodePort(bluePrintNodePort);
+    	 dBean.setDataBrokerTargetPort(dataBrokerTargetPort);
+    	 dBean.setProbeModelPort(probeModelPort);
+    	 dBean.setProbeNodePort(probeNodePort);
+    	 dBean.setDataBrokerTargetPort(dataBrokerTargetPort);
+    	 dBean.setProbeApiPort(probeApiPort);
+    	 log.debug("probeModelPort "+probeModelPort);
+    	 log.debug("probeNodePort "+probeNodePort);
+    	 log.debug("probeTargetPort "+probeTargetPort);
+    	 log.debug("probeApiPort "+probeApiPort);
+    	 log.debug("dataBrokerModelPort "+dataBrokerModelPort);
+    	 log.debug("dataBrokerNodePort "+dataBrokerNodePort);
+    	 log.debug("dataBrokerTargetPort "+dataBrokerTargetPort);
+    	 log.debug("bluePrintNodePort "+bluePrintNodePort);
+	   	 log.debug("nexusUrlnexusUrl "+nexusUrl);
+	   	 log.debug("solutionId "+solutionId);
+	   	 log.debug("revisionId "+revisionId);
+	   	 log.debug("nexusUrl "+nexusUrl);
+	   	 log.debug("nexusUsername "+nexusUsername);
+	   	 log.debug("nexusPd "+nexusPd);
+	   	 log.debug("cmnDataUrl "+cmnDataUrl);
+	   	 log.debug("cmnDataUser "+cmnDataUser);
+	   	 log.debug("cmnDataPd "+cmnDataPd);
+	   	 log.debug("bluePrintImage "+bluePrintImage);
+	   	 log.debug("bluePrintName "+bluePrintName);
+	   	 log.debug("bluePrintPort "+bluePrintPort);
+	   	 log.debug("KUBE_IP "+kubeIP);
+	   	 log.debug("probeImage "+probeImage);
+	   	 log.debug("probePort "+probePort);
+	   	 log.debug("incrementPort "+incrementPort);
+	   	 log.debug("folderPath "+folderPath);
+	   	 log.debug("singleModelPort "+singleModelPort);
+	   	 log.debug("singleNodePort "+singleNodePort);
+	   	 log.debug("singleTargetPort "+singleTargetPort);
+	   	try{
+	   	  String solutionToolKitType=kubeService.getSolutionCode(solutionId, cmnDataUrl, cmnDataUser, cmnDataPd);
+	   	  if(solutionToolKitType!=null && !"".equals(solutionToolKitType) && "CP".equalsIgnoreCase(solutionToolKitType)){
+	   		log.debug("Single Solution Details Start");
+	   		String imageTag=kubeService.getSingleImageData(solutionId, revisionId, cmnDataUrl, cmnDataUser, cmnDataPd);
+	   		solutionZip=kubeService.singleSolutionDetails(dBean, imageTag, singleModelPort);
+	   		log.debug("Single Solution Details End");
+	   	  }else{
+	   		log.debug("Composite Solution Details Start");
+	   		solutionZip=kubeService.compositeSolutionDetails(dBean);
+	   		log.debug("Composite Solution Deployment End");
+	   	  }
+	   	}catch(Exception e){
+			log.error("getSolutionZip failed", e);
 		}
-		response.setHeader("Content-Disposition", "attachment; filename=solution.zip");
-	    response.getOutputStream().write(solutionZip);
-		log.debug("End simple solution ");
-	}
-	
-	/**
-	    * getCompositeSolutionZip is used for composite model
-	    * @param solutionId 
-	    *           - solutionId for Model 
-	    * @param solutionRevisionId
-	    *           - solutionRevisionId for Model 
-	    * @param response
-	    *           - response return file
-	    * @throws IOException
-	    *             exception thrown
-	    */
-	@RequestMapping(value = "/getCompositeSolutionZip", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public void getCompositeSolutionZip( String solutionId, String solutionRevisionId,HttpServletResponse response) throws IOException {
-		log.debug("getCompositeSolutionZip Start ");
-		byte[] solutionZip=null;
-	    HashMap<String,ByteArrayOutputStream> hmap=new HashMap<String,ByteArrayOutputStream>();
-	    DeploymentBean dBean=new DeploymentBean();
-	    ParseJSON parseJson=new ParseJSON();
-	    List<ContainerBean> contList=null;
-	    
-	    try{
-	    	 String nexusUrl=(env.getProperty(DockerKubeConstants.NEXUS_URL_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_URL_PROP) : "";
-	    	 String nexusUsername=(env.getProperty(DockerKubeConstants.NEXUS_USERNAME_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_USERNAME_PROP) : "";
-	    	 String nexusPd=(env.getProperty(DockerKubeConstants.NEXUS_PD_PROP) != null) ? env.getProperty(DockerKubeConstants.NEXUS_PD_PROP) : "";
-	    	 String cmnDataUrl=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCENDPOINURL_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCENDPOINURL_PROP) : "";
-	    	 String cmnDataUser=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCUSER_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCUSER_PROP) : "";
-	    	 String cmnDataPd=(env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCPD_PROP) != null) ? env.getProperty(DockerKubeConstants.CMNDATASVC_CMNDATASVCPD_PROP) : "";
-	    	 String bluePrintImage=(env.getProperty(DockerKubeConstants.BLUEPRINT_IMAGENAME_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_IMAGENAME_PROP):"";
-	    	 String bluePrintName=(env.getProperty(DockerKubeConstants.BLUEPRINT_NAME_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_NAME_PROP):"";
-	    	 String bluePrintPort=(env.getProperty(DockerKubeConstants.BLUEPRINT_PORT_PROP)!= null) ?env.getProperty(DockerKubeConstants.BLUEPRINT_PORT_PROP):"";
-	    	 String kubeIP=(env.getProperty(DockerKubeConstants.KUBE_IP)!= null) ?env.getProperty(DockerKubeConstants.KUBE_IP):"";
-	    	 String probeImage=(env.getProperty(DockerKubeConstants.PROBEIMAGE_NAME)!= null) ?env.getProperty(DockerKubeConstants.PROBEIMAGE_NAME):"";
-	    	 String probePort=(env.getProperty(DockerKubeConstants.PROBEIMAGE_PORT)!= null) ?env.getProperty(DockerKubeConstants.PROBEIMAGE_PORT):"";
-	    	 String incrementPort=(env.getProperty(DockerKubeConstants.INCREMENT_PORT)!= null) ?env.getProperty(DockerKubeConstants.INCREMENT_PORT):"";
-	    	 String folderPath=(env.getProperty(DockerKubeConstants.FOLDERPATH) != null) ? env.getProperty(DockerKubeConstants.FOLDERPATH) : "";
-	    	 
-	    	 log.debug("nexusUrlnexusUrl "+nexusUrl);
-	    	 log.debug("solutionId "+solutionId);
-	    	 log.debug("solutionRevisionId "+solutionRevisionId);
-	    	 log.debug("nexusUrl "+nexusUrl);
-	    	 log.debug("nexusUsername "+nexusUsername);
-	    	 log.debug("nexusPd "+nexusPd);
-	    	 log.debug("cmnDataUrl "+cmnDataUrl);
-	    	 log.debug("cmnDataUser "+cmnDataUser);
-	    	 log.debug("cmnDataPd "+cmnDataUser);
-	    	 log.debug("bluePrintImage "+bluePrintImage);
-	    	 log.debug("bluePrintName "+bluePrintName);
-	    	 log.debug("bluePrintPort "+bluePrintPort);
-	    	 log.debug("KUBE_IP "+kubeIP);
-	    	 log.debug("probeImage "+probeImage);
-	    	 log.debug("probePort "+probePort);
-	    	 log.debug("incrementPort "+incrementPort);
-	    	 log.debug("folderPath "+folderPath);
-	    	 dBean.setSolutionId(solutionId);
-	    	 dBean.setSolutionRevisionId(solutionRevisionId);
-	    	 dBean.setNexusUrl(nexusUrl);
-	    	 dBean.setNexusUserName(nexusUsername);
-	    	 dBean.setNexusPd(nexusPd);
-	    	 dBean.setCmnDataUrl(cmnDataUrl);
-	    	 dBean.setCmnDataUser(cmnDataUser);
-	    	 dBean.setCmnDataPd(cmnDataPd);
-	    	 dBean.setBluePrintImage(bluePrintImage);
-	    	 dBean.setBluePrintName(bluePrintName);
-	    	 dBean.setBluePrintPort(bluePrintPort);
-	    	 dBean.setKubeIP(kubeIP);
-	    	 dBean.setProbeImage(probeImage);
-	    	 dBean.setProbePort(probePort);
-	    	 dBean.setIncrementPort(incrementPort);
-	    	 dBean.setFolderPath(folderPath);
-	    	 /**Blueprint.json**/
-	    	 ByteArrayOutputStream byteArrayOutputStream=kubeService.getBluePrintNexus(solutionId, solutionRevisionId, cmnDataUrl, cmnDataUser, cmnDataUser, nexusUrl, nexusUsername, nexusPd);
-	    	 log.debug("byteArrayOutputStream "+byteArrayOutputStream);
-	    	 dBean.setBluePrintjson(byteArrayOutputStream.toString());
-	    	 /**Proto file code**/
-	    	 contList=parseJson.getProtoDetails(byteArrayOutputStream.toString());
-	    	 log.debug("contList "+contList);
-	    	 dBean.setContainerBeanList(contList);
-	    	 kubeService.getprotoDetails(dBean.getContainerBeanList(),dBean);
-	    	 log.debug("Proto Details");
-	    	 /**DataBroker**/
-	    	 kubeService.getDataBrokerFile(dBean.getContainerBeanList(), dBean,byteArrayOutputStream.toString());
-	    	 log.debug("DataBrokerFile");
-	    	 /**SolutionYml and Datainfo json**/
-	    	 kubeService.getSolutionYMLFile(dBean,byteArrayOutputStream.toString());
-	    	 log.debug("SolutionYMLFile");
-	    	 /**Deploy.sh **/
-	    	 /**Create Zip**/
-	    	 solutionZip=kubeService.createCompositeSolutionZip(dBean);
-	    	 log.debug("CompositeSolutionZip");
-			}catch(Exception e){
-				log.error("getCompositeSolutionZip failed", e);
-			}
-	    log.debug("getCompositeSolutionZip End ");
 	    response.setHeader("Content-Disposition", "attachment; filename=solution.zip");
 	    response.getOutputStream().write(solutionZip);
+	   	log.debug("End getSolutionZip");
 	}
-
-}
+ }
--- src/main/java/com/dockerKube/service/KubeService.java
+++ src/main/java/com/dockerKube/service/KubeService.java
@@ -37,5 +37,9 @@ public interface KubeService {
 	public void getSolutionYMLFile(DeploymentBean dBean,String jsonString)throws Exception;
 	public byte[] createCompositeSolutionZip(DeploymentBean dBean)throws Exception;
 	public byte[] createSingleSolutionZip(DeploymentBean dBean)throws Exception;
-	public String getSingleSolutionYMLFile(String imageTag,String singleModelPort)throws Exception;
+	public String getSolutionCode(String solutionId,String datasource,String userName,String dataPd) throws Exception;
+	public byte[] singleSolutionDetails(DeploymentBean dBean,String imageTag,String singleModelPort)throws Exception;
+	public byte[] compositeSolutionDetails(DeploymentBean dBean)throws Exception;
+	public String getSingleImageData(String solutionId,String revisionId,String datasource,String userName,String dataPd)throws Exception;
+	
 }
